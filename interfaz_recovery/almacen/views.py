d9121f8a72f26a4f0801abe1792f543117392a68
Ramiro WSL Oficina <ramirobj.25@gmail.com>
2022-08-08 11:04:08 -0500

    mods

--- interfaz_recovery/almacen/views.py
+++ interfaz_recovery/almacen/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'form_sol_material/index.html')
+    return render('Hola Mundo') 
     
